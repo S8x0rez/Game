843e07530e1002b30f93df472887a19ecca3ab67
Akiyama Suito <67818513+S8x0rez@users.noreply.github.com>
2023-06-19 17:00:34 +0900

    Update preJudgeWinner.cpp

--- editing/preJudgeWinner.cpp
+++ editing/preJudgeWinner.cpp
@@ -29,30 +29,30 @@ void GameBoard::printBoard(void){
 	cout << endl;
 }
 
-node_t* GameBoard::movePieace(node_t *node, int &turn, int posi){
-	node_t nextNode = *node;
+bool GameBoard::movePieace(node_t node, node_t *nextNode, int &turn, int posi){
+	*nextNode = node;
 	turn++;
 
 	if(turn % 2 == 1){
-		if(node->board[posi] == 1){
-			for(int i = posi + 1; i < node->board.size();i++){
-				if(node->board[i] == 0){
+		if(node.board[posi] == 1){
+			for(int i = posi + 1; i < node.board.size();i++){
+				if(node.board[i] == 0){
 					num++;
-					iter_swap(nextNode.board.begin()+posi, NextNode.board.begin()+i);
-					nextNode.sumMoveDistance.first += i-posi;
-					return &nextNode;
+					iter_swap(nextNode->board.begin()+posi, nextNode->board.begin()+i);
+					nextNode->sumMoveDistance.first += i-posi;
+					return true;
 				}
 				//else if(i == node->board.size()-1) turn--;
 			}
 		}
 	}
 	else{
-		if(node->board[posi] == 2){
+		if(node.board[posi] == 2){
 			for(int i = posi - 1; i >= 0;i--){
-				if(node->board[i] == 0){
-					iter_swap(nextNode.board.begin()+posi, nextNode.board.begin()+i);
-					nextNode.sumMoveDistance.second += posi - i;
-					return &nextNode;
+				if(node.board[i] == 0){
+					iter_swap(nextNode->board.begin()+posi, nextNode->board.begin()+i);
+					nextNode->sumMoveDistance.second += posi - i;
+					return true;
 				}
 				//else if(i == 0) turn--;
 			}
@@ -62,27 +62,61 @@ node_t* GameBoard::movePieace(node_t *node, int &turn, int posi){
 	return &nextNode;
 }
 
-void GameBoard::makeAllNode(){
-	node_t *node;
-	node_t *nextNode;
-	int posi = 0;
+void GameBoard::addNode(node_t *node, node_t *nextNode, int posi){
+		tree.push_back(nextNode);
+		node->next[posi] = tree.size() - 1;
+}
 
-	while(1){
-		for(int i = 0;i < 3;){
-			int j = 0;
-			for(int k = 0;k < 9;k++){
-				if()
-					nextNode = movePieace(node, turn, )
+void GameBoard::makeNode(node_t *node){
+	node_t *nextNode;
+	bool opt;
+
+	for(int i = 0;i < 3;i++){
+		int j = 0;
+		for(int k = 0;k < 9;k++){
+			if(turn % 2 == 1){
+				if(node->board[k] == 1){
+					if(i == j){
+						opt = movePieace(*node, nextNode, turn, k);
+						addNode(node, nextNode, i);
+						break;
+					}
+					else j++;
+				}
+			}
+			else if(turn % 2 == 0){
+				if(node->board[8-k] == 2){
+					if(i == j){
+						opt = movePieace(*node, nextNode, turn, 8-k);
+						addNode(node, nextNode, i);
+						break;
+					}
+					else j++;
+				}
 			}
 		}
 	}
 }
 
-void GameBoard::judgeWinner(node_t *node){
-	if(node->sumMoveDistance.first == GoalDistance) cout << "1 win" << endl;
-	if(node->sumMoveDistance.second == GoalDistance) cout << "2 win" << endl;
+void GameBoard::makeAllNode(){
+	node_t *node;
+	vector<node_t *> way{tree[0]};
 
-	return;
+	while(1){
+		node = way[way.size() - 1];
+		makeNode(node);
+		// 再帰関数
+
+		if(!judgeWinner(*node)) break;
+	}
+}
+
+
+bool GameBoard::judgeWinner(node_t node){
+	if(node.sumMoveDistance.first == GoalDistance) cout << "1 win" << endl;
+	if(node.sumMoveDistance.second == GoalDistance) cout << "2 win" << endl;
+	// どちらかの移動距離の合計が18*rowになったらtrue
+	return (node.sumMoveDistance.first == GoalDistance || node.sumMoveDistance.second == GoalDistance)? true: false;
 }
 
 /*void GameBoard::movePieace(){
